27c6526aed1205f421210eed2a6d58527c0a193d
NisrineDev <lasfernisrine@gmail.com>
2024-02-02 16:17:40 +0100

    commit

--- employee_manager/urls.py
+++ employee_manager/urls.py
@@ -7,8 +7,8 @@ from django.conf.urls.static import static
 
 
 urlpatterns = [
-    path('', admin.site.urls),
-    #path('', include('home.urls')),
+    path('admin/', admin.site.urls),
+    path('', include('home.urls')),
     
 ]
 
