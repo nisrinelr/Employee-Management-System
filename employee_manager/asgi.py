a20ff37b7716e81453663515ad5976e834aca855
NisrineDev <lasfernisrine@gmail.com>
2024-02-02 10:39:16 +0100

    Deployment settings

--- employee_manager/asgi.py
+++ employee_manager/asgi.py
@@ -11,7 +11,7 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'tailAdmin.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'employee_manager.settings')
 
 application = get_asgi_application()
 
