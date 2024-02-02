693a4ca3f8559b8355074626f954a848482806db
NisrineDev <lasfernisrine@gmail.com>
2024-02-02 03:47:49 +0100

    host configuration

--- tailAdmin/settings.py
+++ tailAdmin/settings.py
@@ -10,7 +10,7 @@ SECRET_KEY = 'django-insecure-@2soi&jx2&z7s3@45uhkkz%ig9s3@-1+-^ky6%y@t-qf2&(7xb
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.vercel.app']
 
 # Application definition
 INSTALLED_APPS = [
