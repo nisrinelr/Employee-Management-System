ffee1a1006eff1a00dcacfe4f3e705139bc04e47
LASFER Nisrine <58151588+nisrinelr@users.noreply.github.com>
2024-02-02 02:42:44 +0100

    Delete employee directory

--- employee/Asset/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-from Authentication.models import Employee
-# Create your models here.
-
-class Asset(models.Model):
-    name = models.CharField(max_length=100)
-    description = models.TextField()
-    purchase_date = models.DateField()
-    assigned_to = models.ForeignKey(Employee, on_delete=models.SET_NULL, null=True, blank=True)
--- employee/Attendance/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-from Authentication.models import Employee
-from Authentication.choices import ATTENDANCE_STATUS
-# Create your models here.
-
-class Attendance(models.Model):
-    employee = models.ForeignKey(Employee, on_delete=models.CASCADE)
-    date = models.DateField()
-    status = models.CharField(max_length=10, choices=ATTENDANCE_STATUS)
