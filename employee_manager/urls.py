2983ac76e5aaae81c0fc953c73e60b3c1bca4055
NisrineDev <lasfernisrine@gmail.com>
2024-02-02 14:34:36 +0100

    editing urls.py

--- employee_manager/urls.py
+++ employee_manager/urls.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
 from django.urls import path, include
-
+from django.conf import settings
+from django.conf.urls import url
+from django.views.static import serve
+from django.conf.urls.static import static
 
 
 
@@ -9,3 +12,5 @@ urlpatterns = [
     path('', include('home.urls')),
 
 ]
+
+urlpatterns =+ static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
