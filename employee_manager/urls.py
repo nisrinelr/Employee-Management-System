6d2b9caaeea1b19aeed7e5b490320c8eb552928d
NisrineDev <lasfernisrine@gmail.com>
2024-02-13 17:09:18 +0100

    Prepare for deployement

--- employee_manager/urls.py
+++ employee_manager/urls.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from django.urls import path, include, re_path
 from django.conf import settings
 from django.views.static import serve
-from django.conf.urls.static import static
+from django.conf.urls import static
 
 urlpatterns = [
     path('', admin.site.urls),
