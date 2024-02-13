7c53594dea2931ad852007593eb1d286add02b0d
NisrineDev <lasfernisrine@gmail.com>
2024-02-13 16:13:55 +0100

    Prepare for deployement

--- authentication/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path, include
-from .views import generate_employee_report
-from . import views
-urlpatterns = [
-    path('generateReport/<int:employee_id>', views.generate_employee_report, name='generateReports'),
-]
--- authentication/views.py
+++ authentication/views.py
@@ -1,10 +1,4 @@
 from django.shortcuts import render
-
 # Create your views here.
-from django.http import FileResponse
-from .models import Employee
-from attendance.models import Attendance
-from tasks.models import Task
-from assets.models import Asset
-from payroll.models import Salary
+
 
