d42da6747a0eaddb31e11d8db9d970de2279dcc8
NisrineDev <lasfernisrine@gmail.com>
2024-02-14 15:18:44 +0100

    Prepare for deployement

--- employee_manager/urls.py
+++ employee_manager/urls.py
@@ -6,6 +6,6 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('', admin.site.urls),
 ]
-if settings.DEBUG == False:
+if settings.DEBUG:
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
